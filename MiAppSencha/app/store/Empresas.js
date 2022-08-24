31bd665d4b554bd1d01eb75f079797b74085557b
Kevin Oliva <kevinoliva1.73@gmial.com>
2022-08-24 09:08:34 -0600

    Subiendo el proyecto

---
+++ MiAppSencha/app/store/Empresas.js
@@ -0,0 +1,15 @@
+Ext.define('MiAppSencha.store.Empresas',{
+    extend: 'Ext.data.Store',
+    storeId: 'empresas-listado',
+    alias:'store.empresas',
+    model:'MiAppSencha.model.Empresa',
+    requires: ['Ext.data.proxy.JsonP'],
+    proxy:{
+        type: 'jsonp',
+        url: Ext.manifest.url_backend+'empresas',
+        reader: {
+            type: 'json'
+        }
+    },
+    autoLoad: true
+});
